1186bc28fbda6ecb0c8dadce59e977bb4961737c
Isac Yevisa <isacyevisa@gmail.com>
2025-09-11 12:38:55 +0300

    created serializers for each of our app

---
+++ saccosphere-project/saccosphere_backend/saccomanagement/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers  
+from .models import Management
+from accounts.serializers import SaccoSerializer
+
+class ManagementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Management
+        fields =  ['id', 'sacco', 'management']
+        read_only_fields = ['id']
+        
+class ManagementDetailSerializer(serializers.ModelSerializer):
+    sacco = SaccoSerializer(read_only=True)
+    
+    class Meta:
+        model = Management
+        fields = ['id', 'sacco', 'management']
+        read_only_fields = ['id']        
+        
