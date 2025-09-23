b533d1a2c4def362cb236a6b1223f9130b656c7f
Isac Yevisa <isacyevisa@gmail.com>
2025-09-23 12:37:16 +0300

    exposed my API for frontend and API documentation

--- saccosphere-project/saccosphere_backend/saccosphere_backend/urls.py
+++ saccosphere-project/saccosphere_backend/saccosphere_backend/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SaccoSphere API",
+        default_version="v1",
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +35,5 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/services/', include('services.urls')),
     path('api/payments/', include('payments.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
