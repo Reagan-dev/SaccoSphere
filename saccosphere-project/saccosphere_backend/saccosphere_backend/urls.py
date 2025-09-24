9203c834aa174e48539aa1d83d2e5ae9f601f01c
Isac Yevisa <isacyevisa@gmail.com>
2025-09-24 08:49:44 +0300

    refinements to the app

--- saccosphere-project/saccosphere_backend/saccosphere_backend/urls.py
+++ saccosphere-project/saccosphere_backend/saccosphere_backend/urls.py
@@ -20,6 +20,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.http import HttpResponse
 
+
+
 schema_view = get_schema_view(
     openapi.Info(
         title="SaccoSphere API",
@@ -38,4 +40,5 @@ urlpatterns = [
     path('api/payments/', include('payments.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('', lambda request: HttpResponse("Welcome to SaccoSphere API 🚀")),
+    
 ]
