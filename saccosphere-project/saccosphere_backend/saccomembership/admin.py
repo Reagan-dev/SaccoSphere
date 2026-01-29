7e8e1ed26c3a1ef3e6c1d796f26337555db80bd4
Reagan-dev <reagansimiyu38@gmail.com>
2026-01-29 15:46:27 +0300

    modified saccomembership admins.py

--- saccosphere-project/saccosphere_backend/saccomembership/admin.py
+++ saccosphere-project/saccosphere_backend/saccomembership/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Membership
+from .models import Membership, SaccoField, MembershipFieldData
 
 # using @admin.register decorator to register the Membership model with custom admin interface
 @admin.register(Membership)
@@ -13,3 +13,27 @@ class MembershipAdmin(admin.ModelAdmin):
         ('Membership Info', {'fields': ('user', 'sacco', 'status', 'is_active')}),
         ('Timestamps', {'fields': ('date_joined', 'id')}),
     )
+
+@admin.register(SaccoField)
+class SaccoFieldAdmin(admin.ModelAdmin):
+    list_display = (
+        "field_label",
+        "field_key",
+        "field_type",
+        "sacco",
+        "required",
+        "order",
+    )
+    list_filter = ("sacco", "field_type")
+    search_fields = ("field_label", "field_key")
+    ordering = ("order",)
+
+
+@admin.register(MembershipFieldData)
+class MembershipFieldDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "membership",
+        "sacco_field",
+        "value",
+    )
+    list_filter = ("sacco_field",)
