f21eed776665eb0cc905e195bcd03bd54612f276
Isac Yevisa <isacyevisa@gmail.com>
2026-05-05 08:31:57 +0300

    i deleted

--- saccosphere-project/saccosphere_backend/saccomembership/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import MembershipViewSet, SaccoFieldViewSet 
-
-router = DefaultRouter()
-router.register(r'memberships', MembershipViewSet, basename='membership')
-router.register(r'sacco_fields', SaccoFieldViewSet, basename='sacco-fields')
-
-urlpatterns = router.urls
