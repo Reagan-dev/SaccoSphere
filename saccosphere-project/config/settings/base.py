e8aaf0c823bf8aae5ea6459b1062984759c19e4a
Reagan-dev <reagansimiyu38@gmail.com>
2026-05-08 11:29:39 +0300

    feat: updated config to py.

--- saccosphere-project/config/settings/base.py
+++ saccosphere-project/config/settings/base.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'config.middleware.SaccoContextMiddleware',
+    'saccomanagement.middleware.SaccoContextMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
