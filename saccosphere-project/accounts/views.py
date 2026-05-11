ed566e9de428165cb0a1baa52aa9de3b748da4a8
Reagan-dev <reagansimiyu38@gmail.com>
2026-05-11 12:51:33 +0300

    fix: solved import error.

--- saccosphere-project/accounts/views.py
+++ saccosphere-project/accounts/views.py
@@ -14,6 +14,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
 
 from config.response import StandardResponseMixin
 
