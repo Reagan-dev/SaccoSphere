b7a90dd9aeac403cadbb32e758ba96cde1d12605
Reagan-dev <reagansimiyu38@gmail.com>
2026-06-23 20:05:46 +0300

    fix: added the correct migration dependancy.

--- saccosphere-project/accounts/migrations/0007_alter_user_id_to_uuid.py
+++ saccosphere-project/accounts/migrations/0007_alter_user_id_to_uuid.py
@@ -3,7 +3,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0007_alter_user_id_to_uuid.py'),
+        ('accounts', '0006_alter_otptoken_user'),
     ]
 
     operations = [
