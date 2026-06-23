53bebfd2727254ada39e9f1f3829d6bdd03777b5
Reagan-dev <reagansimiyu38@gmail.com>
2026-06-23 19:30:37 +0300

    fixed database configuration.

--- saccosphere-project/config/settings/base.py
+++ saccosphere-project/config/settings/base.py
@@ -91,8 +91,11 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
+    "default": dj_database_url.parse(
+        config(
+            "DATABASE_URL",
+            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        ),
         conn_max_age=600,
     ),
 }
