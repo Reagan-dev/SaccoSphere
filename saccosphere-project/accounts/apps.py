25f6d5e9aff1ad736a2e1c7d42aa7beff06a8b5e
Reagan-dev <reagansimiyu38@gmail.com>
2026-08-19 09:57:40 +0300

    feat: updated accounts apps to contain AppConfig.ready()

--- saccosphere-project/accounts/apps.py
+++ saccosphere-project/accounts/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        """Run app-specific startup checks."""
+        # Prevent OAUTH_MOCK=True in production
+        if not settings.DEBUG and settings.OAUTH_MOCK:
+            raise ImproperlyConfigured(
+                'OAUTH_MOCK=True is not allowed when DEBUG=False. '
+                'Set OAUTH_MOCK=False in your Railway environment variables '
+                'before deploying to production.'
+            )
